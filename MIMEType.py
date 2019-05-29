fc24375c4647a35486d4afc3f731686618ce20a5
ACaron <43013348+Bidartarra@users.noreply.github.com>
2019-05-29 16:04:03 -0600

    Final version

--- MIMEType.py
+++ MIMEType.py
@@ -12,37 +12,27 @@ for i in range(n):
     # ext: file extension
     # mt: MIME type.
     ext, mt = input().split()
-    mimedict = {**mimedict, ext: mt}
+    mimedict = {**mimedict, '.' + ext.lower(): mt} #lower casing the extension to ease comparison
     
-print(mimedict)
+#print(mimedict)
 for i in range(q):
     fname = input()  # One file name per line.
-    fnameinv = fname[::-1] #inverting the string to find the last extension
+    fnameinv = fname[::-1].lower() #inverting the string to find the last extension and lower casing it
     for k in range(0, len(fnameinv)):
         if fnameinv[k] == '.':
-            extensioninv = fnameinv[0:k] #create a substring with the reversed extension
+            extensioninv = fnameinv[0:k+1] #create a substring with the reversed extension
             extension = extensioninv[::-1]
-    print(extension)
-    #if extension == '':
-        #print('UNKNOWN')
-    #elif extension in mimedict:
-        #print(mimedict[extension])
-    #else:
-        #print('UNKNOWN')
+            break #exiting the loop after the first dot is found
+        elif '.' not in fname:
+            extension = ''
             
-            
-    #if ext in fname:
-        #print(mt)
-        #print(ext)
-    #else:
-        #print("unknown")
-#print(mt)
-#print(ext)
-
-
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr)
-
-
-# For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
-#print( mt)
+    #print(extension)
+    if extension == '':
+        print('UNKNOWN')
+    elif extension == '.':
+        print('UNKNOWN')
+    elif extension in mimedict:
+        print(mimedict[extension])
+    else:
+        print('UNKNOWN')
+    extension == '' #emptying the variable
